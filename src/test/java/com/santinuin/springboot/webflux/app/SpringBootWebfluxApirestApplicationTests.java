6c121b951d99bdcc7938bf100f238c2b54b6f96e
Santiago Nuñez Ingas <santiago.nunez.ingas@gmail.com>
2023-06-29 17:32:45 -0300

    First Test

--- src/test/java/com/santinuin/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/com/santinuin/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -1,13 +1,29 @@
 package com.santinuin.springboot.webflux.app;
 
+import com.santinuin.springboot.webflux.app.models.documents.Producto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootWebfluxApirestApplicationTests {
 
+	@Autowired
+	private WebTestClient client;
+
 	@Test
-	void contextLoads() {
+	void listarTest() {
+
+		client.get()
+				.uri("/api/v2/productos")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBodyList(Producto.class)
+				.hasSize(9);
 	}
 
 }
